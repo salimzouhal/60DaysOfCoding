2a516ebb6834ee7eba72957eb659dfaa08b5e004
Salim Zouhal <szouhal@gmail.com>
2023-12-02 18:21:08 +0100

    Added some code changes related to the Moving Average Strategy

--- Day2_MovingAverageCrossoverStrategy/moving_average.py
+++ Day2_MovingAverageCrossoverStrategy/moving_average.py
@@ -3,10 +3,9 @@ import numpy as np
 from Day1_HistoricalStockPriceDownloader.stock_price import DataScraper
 
 
-class MovingAverageStrategy:
-    def __init__(self, ticker, period, criteria='Close'):
+class TickerPrice:
+    def __init__(self, ticker, criteria='Close'):
         self.ticker = ticker
-        self.period = period
         self.apikey = 'TGQVZHVO92AP5GWD'
         self.criteria = criteria
 
@@ -19,17 +18,29 @@ class MovingAverageStrategy:
         df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
         return df
 
-    def get_average(self):
-        df = self.get_prices()
-        return np.mean(df.iloc[:self.period][self.criteria])
 
+class MovingAverageStrategy():
+    def __init__(self, ticker, period_low, period_high, criteria='Close'):
+        self.ticker = ticker
+        self.period_low = period_low
+        self.period_high = period_high
+        self.criteria = criteria
 
+    def build_prices(self):
+        ticker_prices = TickerPrice(self.ticker, self.criteria)
+        df = ticker_prices.get_prices()
+        return df
+
+    def buy_signal(self):
+        prices = self.build_prices()
+        ma_low = prices.iloc[:self.period_low][self.criteria].values.astype('float').mean()
+        ma_high = prices.iloc[:self.period_high][self.criteria].values.astype('float').mean()
+        return ma_low > ma_high
 
 
 if __name__ == "__main__":
-    mas = MovingAverageStrategy('AAPL', 50)
-    print(mas.get_prices())
-    print(mas.get_average())
+    mas = MovingAverageStrategy('AAPL', 50, 200)
+    print(mas.buy_signal())
 
 
 
